1042bcec1e2c5f55753189275d300b820ab4a8ab
Aleksandra Radovanov <sandricasekica@gmail.com>
2024-02-02 12:32:32 +0100

    Kreiran Spring Boot projekat

---
+++ backend/src/main/java/com/isa/medical_equipment/MedicalEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.isa.medical_equipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalEquipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalEquipmentApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/isa/medical_equipment/MedicalEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.medical_equipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalEquipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
